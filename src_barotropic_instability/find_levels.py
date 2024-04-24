6631c2a08532e0f155decd3499d0a91ba09401b6
daniloceano <danilo.oceano@gmail.com>
2024-04-24 13:04:48 -0300

    progress bar

--- src_barotropic_instability/find_levels.py
+++ src_barotropic_instability/find_levels.py
@@ -6,7 +6,7 @@
 #    By: daniloceano <danilo.oceano@gmail.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/23 15:00:18 by daniloceano       #+#    #+#              #
-#    Updated: 2024/04/23 16:21:15 by daniloceano      ###   ########.fr        #
+#    Updated: 2024/04/24 13:04:09 by daniloceano      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,18 +21,8 @@ import matplotlib.pyplot as plt
 from glob import glob
 from concurrent.futures import ProcessPoolExecutor
 import seaborn as sns
+from tqdm import tqdm  # Import tqdm for the progress bar
 
-# You need to convert column names to numeric where possible, then adjust them.
-def adjust_columns(col):
-    try:
-        # Convert the column name to float, then divide
-        new_col = float(col) / 100
-        return new_col
-    except ValueError:
-        # Return the original column name if it's not a number
-        return col
-
-# Function to read a file and return the DataFrame without the time column
 def read_and_process(file_path):
     df = pd.read_csv(file_path, index_col=0)
     df.columns = [float(col) / 100 for col in df.columns]
@@ -47,10 +37,11 @@ file_paths = glob('../../LEC_Results_energetic-patterns/*/Ck_level.csv')
 
 # Step 2: Use ProcessPoolExecutor to read files in parallel
 with ProcessPoolExecutor() as executor:
-    data_frames = list(executor.map(read_and_process, file_paths))
+    # Wrap file_paths with tqdm to create a progress bar
+    results = list(tqdm(executor.map(read_and_process, file_paths), total=len(file_paths), desc="Processing Files"))
 
 # Step 3: Combine the data into a single DataFrame
-combined_df = pd.concat(data_frames)
+combined_df = pd.concat(results)
 
 # Step 4: Melt the DataFrame to make it compatible with seaborn's violin plot function
 melted_df = combined_df.melt(var_name='Vertical Level', value_name='Ck Value')
@@ -66,4 +57,5 @@ ax.ticklabel_format(style='scientific', axis='y', scilimits=(0,0))
 ax.tick_params(axis='both', labelsize=12)
 plt.tight_layout()
 
+# Save the figure
 plt.savefig(os.path.join(figures_dir, 'violin_plot_ck_levels.png'))
