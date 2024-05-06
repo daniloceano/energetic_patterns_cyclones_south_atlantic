053b152147bf91156b1e110f0f8caceb37adcdf4
daniloceano <danilo.oceano@gmail.com>
2024-05-06 11:40:16 -0300

    LPS for fixed results

--- src_test_fixed_x_mobile_domain/plot_lps_fixed_all_systems.py
+++ src_test_fixed_x_mobile_domain/plot_lps_fixed_all_systems.py
@@ -6,7 +6,7 @@
 #    By: daniloceano <danilo.oceano@gmail.com>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/05 18:56:28 by daniloceano       #+#    #+#              #
-#    Updated: 2024/05/06 09:57:59 by daniloceano      ###   ########.fr        #
+#    Updated: 2024/05/06 11:37:10 by daniloceano      ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,48 +17,83 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from lorenz_phase_space.phase_diagrams import Visualizer
 
-def plot_all_systems_by_region_season(systems_energetics, id_list_directory, season, region, output_directory):
+def read_life_cycles(base_path):
+    """
+    Reads all CSV files in the specified directory and collects DataFrame for each system.
+    """
+    systems_energetics = {}
+
+    for filename in tqdm(os.listdir(base_path), desc="Reading CSV files"):
+        if filename.endswith('.csv'):
+            file_path = os.path.join(base_path, filename)
+            system_id = filename.split('_')[0]
+
+            # Read the CSV file
+            try:
+                df = pd.read_csv(file_path)
+                systems_energetics[system_id] = df
+            except Exception as e:
+                print(f"Error processing {filename}: {e}")
+    
+    return systems_energetics
+
+def plot_system(lps, df):
+    """
+    Plots the Lorenz Phase Space diagram for a single system
+    """
+    # Generate the phase diagram
+    lps.plot_data(
+        x_axis=df['Ck'],
+        y_axis=df['Ca'],
+        marker_color=df['Ge'],
+        marker_size=df['Ke']
+    )
+
+def plot_all_systems_by_region_season(systems_energetics, output_directory):
 
     # Initialize the Lorenz Phase Space plotter
     lps = Visualizer(LPS_type='mixed', zoom=False)
-
-    ids_file = glob(f"{id_list_directory}/*{region}*{season}.csv")
-    print(f"IDs file: {ids_file}")
-    track_ids = pd.read_csv(ids_file[0])
+    lps_zoom = Visualizer(LPS_type='mixed', zoom=True)
 
     # Plot each system onto the Lorenz Phase Space diagram
     for system_id, df in tqdm(systems_energetics.items(), desc="Plotting systems"):
-        system_id = int(system_id)
-        if system_id in track_ids.values:
-            plot_system(lps, df)
+        plot_system(lps, df)
+        plot_system(lps_zoom, df)
+
+    # Ajut limits for zoomed plot
+    lps_zoom.ax.set_xlim(-100, 60)
+    lps_zoom.ax.set_ylim(-7, 20)
     
     # Save the final plot
-    plot_filename = f'lps_all_systems_{region}_{season}.png'
+    plot_filename = f'lps_all_systems_fixed.png'
+    plot_zoom_filename = f'lps_zoom_all_systems_fixed.png'
+
     plot_path = os.path.join(output_directory, plot_filename)
-    plt.savefig(plot_path)
+    plot_zoom_path = os.path.join(output_directory, plot_zoom_filename)
+
+    lps.fig.savefig(plot_path)
+    lps_zoom.fig.savefig(plot_zoom_path)
+
     plt.close()
-    print(f"Final plot saved to {plot_path}")
+    print(f"Saved plots to {plot_path}, {plot_zoom_path}")
 
 def main():
-    base_path = '../csv_database_energy_by_periods'
-    output_directory = '../figures_lps/test_fixed_framework/all_systems/'
-
-    tracks_filtered = pd.read_csv('../tracks_SAt_filtered/tracks_SAt_filtered.csv')
-    tracks_filtered_with_periods = pd.read_csv('../tracks_SAt_filtered/tracks_SAt_filtered_with_periods.csv')
+    base_path = '../csv_fixed_framework_database_energy_by_periods'
+    output_directory = '../figures_lps/test_fixed_framework/'
 
     os.makedirs(output_directory, exist_ok=True)
-
-    id_list_directory = '../csv_track_ids_by_region_season'
 
     # Read the energetics data for all systems
     systems_energetics = read_life_cycles(base_path)
 
-    clusters_to_use = ["ARG_DJF_cl_2", "ARG_JJA_cl_1",
-                       "LA-PLATA_DJF_cl_2", "LA-PLATA_JJA_cl_2",
-                       "SE-BR_DJF_cl_2", "SE-BR_JJA_cl_3"]
+    plot_all_systems_by_region_season(systems_energetics, output_directory)
+
+    # clusters_to_use = ["ARG_DJF_cl_2", "ARG_JJA_cl_1",
+    #                    "LA-PLATA_DJF_cl_2", "LA-PLATA_JJA_cl_2",
+    #                    "SE-BR_DJF_cl_2", "SE-BR_JJA_cl_3"]
     
-    for cluster in clusters_to_use:
-        plot_all_systems_by_region_season(systems_energetics, id_list_directory, cluster, output_directory)
+    # for cluster in clusters_to_use:
+    #     plot_all_systems_by_region_season(systems_energetics, id_list_directory, cluster, output_directory)
 
 if __name__ == "__main__":
     main()
